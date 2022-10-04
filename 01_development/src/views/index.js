5b8ac4ecd812e051b6f66af8573ae932aed09ff0
Portia Senior <pocamsenior@gmail.com>
2022-10-04 12:01:46 -0400

    add page views

---
+++ 01_development/src/views/index.js
@@ -0,0 +1,6 @@
+import * as home from './homeView';
+import * as about from './aboutView';
+import * as projects from './projectsView';
+import * as senor from './senorView';
+
+export { home, about, projects, senor };
